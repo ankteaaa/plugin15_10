6cff563ceab62bf28ab129f9af1d011c9788ee0f
ankteaaa <90596636+ankteaaa@users.noreply.github.com>
2021-10-15 08:15:36 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+/** 
+* Plugin name: Popup with JavaScript
+* Description: A popup that can be turned off with JavaScript
+* Plugin URI: http://localhost
+* Version: 1.0
+* Author: Andreas Kure Thorngreen
+* Author http://localhost
+* License: GPL2
+*/ 
+?>
